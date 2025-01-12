00e34bbd7a4cc2eb30c3902e00d2dcb9287db295
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2025-01-12 22:56:51 +0200

    Update ATTStatus.hx

--- extension/admob/ios/ATTStatus.hx
+++ extension/admob/ios/ATTStatus.hx
@@ -1,11 +1,35 @@
 package extension.admob.ios;
 
-// https://stackoverflow.com/questions/63499520/app-tracking-transparency-how-does-effect-apps-showing-ads-idfa-ios14/63522856#63522856
+/**
+ * Enum representing the different statuses for App Tracking Transparency (ATT) in iOS.
+ * These statuses indicate the user's permission for tracking within apps, as per the App Tracking Transparency framework introduced in iOS 14.
+ *
+ * @see https://stackoverflow.com/questions/63499520/app-tracking-transparency-how-does-effect-apps-showing-ads-idfa-ios14/63522856#63522856
+ */
 enum abstract ATTStatus(String) from String to String
 {
-	final NOT_DETERMINED = 'NOT_DETERMINED';
-	final RESTRICTED = 'RESTRICTED';
-	final DENIED = 'DENIED';
-	final AUTHORIZED = 'AUTHORIZED';
-	final UNKNOWN = 'UNKNOWN';
+	/**
+	 * The user has not yet made a choice regarding tracking.
+	 */
+	final NOT_DETERMINED = "NOT_DETERMINED";
+
+	/**
+	 * The user's tracking permission is restricted.
+	 */
+	final RESTRICTED = "RESTRICTED";
+
+	/**
+	 * The user has denied permission for tracking.
+	 */
+	final DENIED = "DENIED";
+
+	/**
+	 * The user has authorized tracking.
+	 */
+	final AUTHORIZED = "AUTHORIZED";
+
+	/**
+	 * The status is unknown.
+	 */
+	final UNKNOWN = "UNKNOWN";
 }
