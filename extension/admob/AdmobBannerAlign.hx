9761edbd8e1bcd180a59f783287830a5997c1b29
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2025-01-12 22:55:36 +0200

    Update AdmobBannerAlign.hx

--- extension/admob/AdmobBannerAlign.hx
+++ extension/admob/AdmobBannerAlign.hx
@@ -1,14 +1,36 @@
 package extension.admob;
 
-// constants are taken from https://developer.android.com/reference/android/view/Gravity
-// you can use your own value for Android, if need more flexibility
+/**
+ * Enum representing the different alignments for AdMob banner ads.
+ * The values in this enum are based on Android's `Gravity` constants, but can be customized for Android if more flexibility is required.
+ *
+ * For Android, the alignment values combine `BOTTOM` or `TOP` with `CENTER_HORIZONTAL`.
+ * For iOS, the alignment is represented with simpler integer values.
+ *
+ * Constants are taken from:
+ * @see https://developer.android.com/reference/android/view/Gravity
+ */
 enum abstract AdmobBannerAlign(Int) from Int to Int
 {
 	#if android
-	final BOTTOM = 0x00000050 | 0x00000001; // BOTTOM | CENTER_HORIZONTAL
-	final TOP = 0x00000030 | 0x00000001; // TOP | CENTER_HORIZONTAL
+	/**
+	 * Align the banner to the bottom of the screen with horizontal centering.
+	 */
+	final BOTTOM = 0x00000050 | 0x00000001;
+
+	/**
+	 * Align the banner to the top of the screen with horizontal centering.
+	 */
+	final TOP = 0x00000030 | 0x00000001;
 	#elseif ios
+	/**
+	 * Align the banner to the bottom of the screen.
+	 */
 	final BOTTOM = 0;
+
+	/**
+	 * Align the banner to the top of the screen.
+	 */
 	final TOP = 1;
 	#end
 }
