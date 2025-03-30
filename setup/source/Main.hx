81ab3a73e748a80062396e7ea21e53a5223ac27a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2025-03-30 18:11:55 +0300

    Add `UnityAdapter` back to `iOS`.
    * Testing
    
    * Possible fix?
    
    * Add `ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES `.
    
    * Testing
    
    * This should be the final version.
    
    * Hmm
    
    * fojasoijasdoiasjd

--- setup/source/Main.hx
+++ setup/source/Main.hx
@@ -10,7 +10,9 @@ class Main
 {
 	@:noCompletion
 	private static final URLS:Map<String, String> = [
-		'googlemobileadssdkios.zip' => 'https://dl.google.com/googleadmobadssdk/googlemobileadssdkios.zip'
+		'googlemobileadssdkios.zip' => 'https://dl.google.com/googleadmobadssdk/googlemobileadssdkios.zip',
+		'UnityAds.zip' => 'https://github.com/Unity-Technologies/unity-ads-ios/releases/download/4.14.1/UnityAds.zip',
+		'UnityAdapter-4.14.0.0.zip' => 'https://dl.google.com/googleadmobadssdk/mediation/ios/unity/UnityAdapter-4.14.0.0.zip'
 	];
 
 	@:noCompletion
